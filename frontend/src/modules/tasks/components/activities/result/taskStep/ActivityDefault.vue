644f133dd43581b21e028204cb9c33170e6754b5
Michael Götz <mgoetz1995@gmail.com>
2022-10-30 12:15:56 +0100

    chore: add mini map to the generic task step activity

--- frontend/src/modules/tasks/components/activities/result/taskStep/ActivityDefault.vue
+++ frontend/src/modules/tasks/components/activities/result/taskStep/ActivityDefault.vue
@@ -1,4 +1,9 @@
 <template>
+  <MiniMap
+    v-if="geolocation"
+    :lat="geolocation.latitude"
+    :lng="geolocation.longitude"
+  />
   <BaseList class="measurement-preview">
     <LogListTaskStepItem :log-task-step="taskStep" />
   </BaseList>
@@ -8,11 +13,23 @@
 import LogListTaskStepItem from "@/modules/log/components/LogListTaskStepItem.vue"
 import type { LogTaskStep } from "@/modules/log/models/logTaskStep"
 import { BaseList } from "@michigg/component-library"
+import MiniMap from "@/shared/components/maps/MiniMap.vue"
+import { computed } from "vue"
 
 // Access to task step properties and sensors
-defineProps<{
+const props = defineProps<{
   taskStep: LogTaskStep;
 }>()
+
+const geolocation = computed(() => {
+  const latitude = props.taskStep.results[0].metas.get("latitude") as number
+  const longitude = props.taskStep.results[0].metas.get("longitude") as number
+
+  if (!latitude || !longitude) {
+    return
+  }
+  return {latitude, longitude}
+})
 </script>
 
 <style scoped></style>
