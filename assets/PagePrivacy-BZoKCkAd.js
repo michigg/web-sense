fe6ca64dcf5ca9680886ed01cfca507ec06fc221
michigg <michigg@users.noreply.github.com>
2024-01-30 12:30:10 +0000

    Deploying to gh-pages from @ michigg/web-sense@07a8893dab82de33b9cf1e6c299b358382fe19a1 🚀

---
+++ assets/PagePrivacy-BZoKCkAd.js
@@ -0,0 +1,1 @@
+import{_ as e,e as s,o as a,a as c}from"./index-kORwfdQJ.js";const o={},t={class:"about"},n=c("h1",null,"This is an privacy page",-1),_=[n];function r(i,d){return a(),s("div",t,_)}const f=e(o,[["render",r]]);export{f as default};
