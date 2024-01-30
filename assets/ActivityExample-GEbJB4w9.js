f43ae100888de181ce97179b7c02b39c9b1e97c7
michigg <michigg@users.noreply.github.com>
2024-01-30 11:56:36 +0000

    Deploying to gh-pages from @ michigg/web-sense@f334b6cdc95fc268195abfbc459593a480d54922 🚀

---
+++ assets/ActivityExample-GEbJB4w9.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index-5FRSWYSt.js";const p=t({__name:"ActivityExample",props:{taskStep:{},sensors:{}},emits:["submit"],setup(e,{emit:s}){return(n,m)=>"Content"}});export{p as default};
