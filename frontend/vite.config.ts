6269056736a4c1cf542e5e689f0819b0d73abab2
Michael Götz <michael.goetz@lion5.io>
2024-02-03 16:43:26 +0100

    :sparkles: feat: add screenshot for better install experience

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      devOptions: {
+        enabled: true
+      },
       registerType: "autoUpdate",
       strategies: "generateSW",
       workbox: {
@@ -22,30 +25,46 @@ export default defineConfig({
       manifest: {
         name: "WebSense",
         short_name: "WebSense",
-        description: "Sense everything",
-        theme_color: "#09b6e5",
-        // msTileColor: "#ffffff",
-        // appleMobileWebAppCapable: "yes",
-        // appleMobileWebAppStatusBarStyle: "black",
-        // appleMobileWebAppCache: "yes",
+        description: "WebSense is your crowd sensing buddy. It makes all sensors of the web plattform available for you. So you can measure your environment as you like.",
+        theme_color: "#FF9701",
         orientation: "portrait",
         display: "standalone",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/favicon-32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
+            src: "/favicon-1024.png",
+            sizes: "1024x1024",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/favicon-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "maskable",
           },
         ],
         shortcuts: [
@@ -56,12 +75,65 @@ export default defineConfig({
             url: "/sensors",
             icons: [
               {
-                src: "/img/icons/pwa-192x192.png",
+                src: "/favicon-192.png",
                 sizes: "192x192",
               },
             ],
           },
         ],
+        screenshots: [
+          {
+            "src": "/screenshots/missions.png",
+            "sizes": "745x1400",
+            "type": "image/png",
+            "form_factor": "narrow",
+            "label": "Missionsauswahl"
+          },
+          {
+            "src": "/screenshots/mission.png",
+            "sizes": "745x1400",
+            "type": "image/png",
+            "form_factor": "narrow",
+            "label": "Mission"
+          },
+          {
+            "src": "/screenshots/log.png",
+            "sizes": "745x1400",
+            "type": "image/png",
+            "form_factor": "narrow",
+            "label": "Messungen"
+          },
+          {
+            "src": "/screenshots/log-item.png",
+            "sizes": "745x1400",
+            "type": "image/png",
+            "form_factor": "narrow",
+            "label": "Messung"
+          },
+          {
+            "src": "/screenshots/missions-desktop.png",
+            "sizes": "1989x1400",
+            "type": "image/png",
+            "form_factor": "wide",
+            "label": "Missionsauswahl"
+          },
+        ],
+        share_target: {
+          action: "/log/share-receiver/",
+          method: "POST",
+          enctype: "multipart/form-data",
+          params: {
+            title: "Test",
+            text: "Description",
+            url: "",
+            files: [
+              {
+                "name": "task",
+                "accept": ["text/plain", ".txt"]
+              }
+            ]
+          }
+        }
       },
     }),
   ],
