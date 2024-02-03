3e4f5e7b678e60dbbe00853e9eee91585c0eb531
michigg <michigg@users.noreply.github.com>
2024-02-03 22:54:09 +0000

    Deploy preview for PR 61 🛫

---
+++ pr-preview/pr-61/assets/ActivityExample-9vUvgTJ2.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index-ehfeDtny.js";const p=t({__name:"ActivityExample",props:{taskStep:{},sensors:{}},emits:["submit"],setup(e,{emit:s}){return(n,m)=>"Content"}});export{p as default};
