6181255126f4dea823786f8273a78a83390238c2
michigg <michigg@users.noreply.github.com>
2024-02-03 22:59:16 +0000

    Deploy preview for PR 61 🛫

--- pr-preview/pr-61/registerSW.js
+++ pr-preview/pr-61/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pr-preview/pr-Object/sw.js', { scope: '/pr-preview/pr-Object/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pr-preview/pr-61/sw.js', { scope: '/pr-preview/pr-61/' })})}
