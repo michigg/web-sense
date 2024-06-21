2e0a8d3397a0f48a811ff977febc52bff4f2d2f4
Michael Götz <michael.goetz@lion5.io>
2024-06-21 22:43:15 +0200

    chore: remove prime vue and update deps

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
-import PrimeVue from 'primevue/config';
 
 import App from "./App.vue"
 import router from "./router"
@@ -11,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue, { unstyled: true });
 
 app.mount("#app")
