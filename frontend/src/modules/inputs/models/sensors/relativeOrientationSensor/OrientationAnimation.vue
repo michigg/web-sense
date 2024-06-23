ceb115545ad6afb25e7d42a9381448f7b7e5ab9b
Michael Götz <michael.goetz@lion5.io>
2024-06-23 13:36:34 +0200

    chore:  apply the quaternion with the invert

--- frontend/src/modules/inputs/models/sensors/relativeOrientationSensor/OrientationAnimation.vue
+++ frontend/src/modules/inputs/models/sensors/relativeOrientationSensor/OrientationAnimation.vue
@@ -21,8 +21,9 @@ const threeJsWrapper = ref<HTMLDivElement>()
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
-const geometry = new BoxGeometry(.5, 2, .05);
-const material = new MeshBasicMaterial({color: 0xababab, opacity: 0});
+camera.lookAt(new Vector3(0, 0));
+const geometry = new BoxGeometry(1, 2.5, .05);
+const material = new MeshBasicMaterial({color: 0xababab, opacity: .4, transparent: true});
 const cube = new Mesh(geometry, material);
 scene.add(cube);
 const vectorX = new Vector3( 1, 0, 0 );
@@ -31,9 +32,9 @@ const vectorZ = new Vector3( 0, 0, 1 );
 const arrowHelperX = new ArrowHelper( vectorX.normalize(), new Vector3( 0, 0, 0 ), 1, 0xff0000 );
 const arrowHelperY = new ArrowHelper( vectorY.normalize(), new Vector3( 0, 0, 0 ), 1, 0x00ff00 );
 const arrowHelperZ = new ArrowHelper( vectorZ.normalize(), new Vector3( 0, 0, 0 ), 1, 0x0000ff );
-scene.add(arrowHelperX)
-scene.add(arrowHelperY)
-scene.add(arrowHelperZ)
+cube.add(arrowHelperX)
+cube.add(arrowHelperY)
+cube.add(arrowHelperZ)
 const renderer = new WebGLRenderer();
 
 onMounted(() => {
@@ -42,20 +43,12 @@ onMounted(() => {
   }
   renderer.setSize(threeJsWrapper.value.clientWidth, threeJsWrapper.value.clientHeight);
   threeJsWrapper.value?.appendChild(renderer.domElement)
-  const quaternion = new Quaternion().fromArray(props.quaternion);
-  arrowHelperX.applyQuaternion(quaternion)
-  arrowHelperY.applyQuaternion(quaternion)
-  arrowHelperZ.applyQuaternion(quaternion)
-  cube.applyQuaternion(quaternion)
+  cube.quaternion.fromArray(props.quaternion).invert()
   renderer.render( scene, camera );
 })
 
 watch(() => props.quaternion,(newValue) => {
-  const quaternion = new Quaternion().fromArray(newValue);
-  arrowHelperX.applyQuaternion(quaternion)
-  arrowHelperY.applyQuaternion(quaternion)
-  arrowHelperZ.applyQuaternion(quaternion)
-  cube.applyQuaternion(quaternion)
+  cube.quaternion.fromArray(newValue).invert()
   renderer.render(scene, camera)
 })
 
