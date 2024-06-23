f14ab6a5f13d9a7b10df2ed9b69ef2d74fc9b800
michigg <michigg@users.noreply.github.com>
2024-06-23 10:54:36 +0000

    Deploy preview for PR 64 🛫

---
+++ pr-preview/pr-64/assets/PreviewActivity-pRhzDdQQ.js
@@ -0,0 +1,1 @@
+import{d as n,o,c as s,u as e,M as t}from"./index-BfvkfD6P.js";const u=n({__name:"PreviewActivity",props:{sensor:{}},setup(c){const r=new Error("Sensor konnte nicht gefunden werden. Dies ist der Dummy Sensor.");return(a,i)=>(o(),s(e(t),{error:e(r)},null,8,["error"]))}});export{u as default};
