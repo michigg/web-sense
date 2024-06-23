a34516c16c57732cbf99b8d784baf19b908f8537
michigg <michigg@users.noreply.github.com>
2024-06-23 11:43:32 +0000

    Deploy preview for PR 64 🛫

--- pr-preview/pr-64/assets/ActivityExample-HNh7_N6i.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./index-BfvkfD6P.js";const p=t({__name:"ActivityExample",props:{taskStep:{},sensors:{}},emits:["submit"],setup(e,{emit:s}){return(n,m)=>"Content"}});export{p as default};
